eb9ea9e7d418807b898a4f9fd1a3854f98ad5420
Harith Raghavi Mattupalli <raghavi5321@gmail.com>
2020-08-06 21:33:33 +0530

    Updates

---
+++ precache-manifest.68551045e2168f705a73f7e89d0668ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a645dc7d71332964dd84212231d07aa2",
+    "url": "/sample/index.html"
+  },
+  {
+    "revision": "1e5270150ac8646f00c8",
+    "url": "/sample/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d5a669e8e24082a2b556",
+    "url": "/sample/static/js/2.218537f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sample/static/js/2.218537f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5270150ac8646f00c8",
+    "url": "/sample/static/js/main.6dfa919a.chunk.js"
+  },
+  {
+    "revision": "a509eceae00a8fc49b91",
+    "url": "/sample/static/js/runtime-main.937d243b.js"
+  }
+]);
